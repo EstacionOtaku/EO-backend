25bfa33b7bd19c29ca8203f473326bf3065c1189
JeanfrancoLA <genkoz98@gmail.com>
2022-06-15 23:59:52 -0500

    >>Configuracion e instalacion de Dotenv<<

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from 'express';
+import 'dotenv/config';
 
 const app = express();
-
 //middleware
 app.use(express.json());
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import app from './app.js';
 
+const port = process.env.NODE_PORT;
 
 app.listen(8000, () => {
-    console.log('Express Running... http://localhost:8000');
+    console.log(`Express Running... http://localhost:${port}`);
 });
 
