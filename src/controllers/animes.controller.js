a4034f99d9fe8cc46b29e1125f88e9872a3c9241
Ginepp <ginepp@gmail.com>
2022-06-17 19:18:17 -0500

    Create animes.controller

--- src/controllers/animes.controller.js
+++ src/controllers/animes.controller.js
@@ -1,5 +1,20 @@
+import { animes } from "../models";
+
 class AnimeController {
     constructor() {
-        
+        this.model = animes;
     }
-}
+
+    async getAll(request, response) {
+        try{
+            const records = await this.model.findAll();
+            return response.status(200).json(records);
+        } catch (error){
+            return response.status(500).json({
+                message: error.message,
+            });
+        }
+    }
+} 
+
+module.exports = AnimeController;
